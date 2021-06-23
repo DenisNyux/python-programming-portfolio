0b421a99938b079096929ce8c1895219916d6cde
DenisNyux <54911993+DenisNyux@users.noreply.github.com>
2021-06-23 23:02:21 +0300

    sem5_isr2

---
+++ sem5/1/ИСР/task2/gb_module.py
@@ -0,0 +1,39 @@
+# писал сам, не нашел старой лабы
+import json
+import os
+
+
+class GuestBook:
+
+
+    def make_json(filename = 'data.json'):
+        with open(filename, 'w') as file:
+            file.write('')
+
+    
+    def delete_json(filename = 'data.json'):
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+        os.remove(path)
+
+
+    def rename_json(new_filename, filename = 'data.json'):
+        os.rename(filename, new_filename)
+
+
+    def read_json(filename = 'data.json'):
+        with open(filename, 'r') as file:
+            read_json = json.load(file)
+        return read_json
+
+    
+    def write_json(json_text, filename = 'data.json'):
+        with open(filename, 'w') as file:
+            json.dump(json_text, file, ensure_ascii=False, indent=4)
+
+
+
+# GuestBook.make_json()
+# print(GuestBook.read_json())
+# GuestBook.delete_json('data_copy.json')
+# GuestBook.rename_json('super_data.json')
+
---
+++ sem5/1/ИСР/task2/task2.py
@@ -0,0 +1,7 @@
+from gb_module import GuestBook
+
+
+# GuestBook.make_json()
+# print(GuestBook.read_json())
+# GuestBook.delete_json('data_copy.json')
+GuestBook.rename_json('super_data.json')
