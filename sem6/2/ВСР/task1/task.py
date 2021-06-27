ca3eff37226af6ccaa90a48ee251c73bf3ff2b1b
DenisNyux <54911993+DenisNyux@users.noreply.github.com>
2021-06-27 20:17:43 +0300

    added sem6

---
+++ sem6/2/ВСР/task1/task.py
@@ -0,0 +1,21 @@
+def get_text(filename):
+    with open(filename, 'r') as fl:
+        data = fl.readlines()
+    return data
+
+def count_words(str_lst):
+    total_words = 0
+    for each in str_lst:
+        total_words += len(each)
+    
+    for i in range(len(str_lst)):
+        str_lst[i] = str_lst[i].split(' ')
+    total_array = [i for sublist in str_lst for i in sublist]
+    yield total_words
+    for i in total_array:
+        yield len(i)
+
+
+a = count_words(get_text('text.txt'))
+for each in a:
+    print(each)
